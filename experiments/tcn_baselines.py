61e52a62d98df7a981f68ffb9f60d42fa0f9b308
luis <l.p.silvestrin@vu.nl>
2022-09-01 13:43:38 +0200

    add wandb sweep function

--- experiments/tcn_baselines.py
+++ experiments/tcn_baselines.py
@@ -33,13 +33,11 @@ def train_tcn(train_x, train_y, test_sets, init_dict):
     run = wandb.init(**init_dict)
 
     config = wandb.config
-    # config = init_dict['config']
 
     np.random.seed(config.seed)
     tf.random.set_seed(config.seed)
     random.seed(config.seed)
 
-    test_dict = {}
     if config.transpose_input:
         train_x = np.transpose(train_x, (0, 2, 1))
 
@@ -54,13 +52,19 @@ def train_tcn(train_x, train_y, test_sets, init_dict):
 
     i = Input(shape=(nb_steps, nb_features))
 
-    return_sequences = False if config.tcn2 is None else True
-    dilations1 = np.exp2(np.arange(config.tcn1['dilations'])).astype('int').tolist()
-    # dilations1 = [1,2]
-    m = TCN(kernel_size=config.kernel_size, nb_filters=config.tcn1['filters'], dropout_rate=config.dropout_rate, dilations=dilations1, return_sequences=return_sequences)(i)
-    if config.tcn2 is not None:
-        dilations2 = np.exp2(np.arange(config.tcn2['dilations'])).astype('int').tolist()
-        m = TCN(kernel_size=config.kernel_size, nb_filters=config.tcn2['filters'], dropout_rate=config.dropout_rate, dilations=dilations2, return_sequences=False)(m)
+    return_sequences = True if config.tcn2 else True
+    dilations = np.exp2(np.arange(config.tcn['dilations'])).astype('int').tolist()
+    m = TCN(kernel_size=config.kernel_size,
+            nb_filters=config.tcn['filters'],
+            dropout_rate=config.dropout_rate,
+            dilations=dilations,
+            return_sequences=return_sequences)(i)
+    if config.tcn2:
+        m = TCN(kernel_size=config.kernel_size,
+                nb_filters=config.tcn['filters'],
+                dropout_rate=config.dropout_rate,
+                dilations=dilations,
+                return_sequences=False)(m)
     m = Dense(nb_out, activation='linear')(m)
     model = Model(inputs=[i], outputs=[m])
 
@@ -137,8 +141,8 @@ def gridsearch_tcn_baseline():
             'validation_split': [0.1],
             'early_stop_patience': [15],
             'seed': list(range(5)),
-            'tcn1': grid_tcn,
-            'tcn2': [None] + grid_tcn,
+            'tcn': grid_tcn,
+            'tcn2': [True, False],
             'kernel_size': [2, 3],
             'transpose_input': [True, False]
         }
@@ -160,8 +164,28 @@ def gridsearch_tcn_baseline():
         train_tcn(win_x, win_y, test_data_dict, wandb_init)
 
 
+def wandb_sweep():
+    config = {}
+    wandb_init = dict(
+        project='test2',
+        entity='transfer-learning-tcn',
+        reinit=False,
+        config=config
+    )
+    data_dict = load_preproc_data(name='src')
+    win_x = data_dict['win_x_train']
+    win_y = data_dict['y_train']
+
+    test_data_dict = {"test": (data_dict['win_x_test'], data_dict['y_test'])}
+    for name in ["tar1", "tar2", "tar3"]:
+        data_dict = load_preproc_data(name=name)
+        test_data_dict[name] = (data_dict['win_x_test'], data_dict['y_test'])
+
+    train_tcn(win_x, win_y, test_data_dict, wandb_init)
+
+
 if __name__ == '__main__':
-    gridsearch_tcn_baseline()
+    wandb_sweep()
 
 
 
