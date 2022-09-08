0f6358c5e846356dde228fdeed257043fa9139db
luis <l.p.silvestrin@vu.nl>
2022-09-08 17:01:43 +0200

    add option to train baseline using concatenated src and target dataset

--- experiments/tcn_baselines.py
+++ experiments/tcn_baselines.py
@@ -118,6 +118,60 @@ def train_and_log_baselines():
                   save_model=False,
                   l2_reg=1e-2,
                   # train_dataset=['src', 'tar1', 'tar2', 'tar3'])
+                  train_dataset='src+tar1')
+
+    wandb_init = dict(
+        project='test2',
+        entity='transfer-learning-tcn',
+        reinit=False,
+        config=config
+    )
+
+    # grid = list(ParameterGrid(config))
+
+    test_data_dict = dict()
+    for name in ["tar1", "tar2", "tar3"]:
+        data_dict = load_preproc_data(name=name)
+        test_data_dict[name] = (data_dict['win_x_test'], data_dict['y_test'])
+
+    wandb.init(**wandb_init)
+    config = wandb.config
+    print(config)
+    # in case of 2 training sets, concatenate them together
+    train_datasets = config['train_dataset'].split('+')
+    data_dict = load_preproc_data(name=train_datasets[0])
+    win_x, win_y = data_dict['win_x_train'], data_dict['y_train']
+
+    if len(train_datasets) > 1:
+        data_dict = load_preproc_data(name=train_datasets[1])
+        win_x = np.concatenate([win_x, data_dict['win_x_train']])
+        win_y = np.concatenate([win_y, data_dict['y_train']])
+
+    test_data_dict["test"] = (data_dict['win_x_test'], data_dict['y_test'])
+
+    wandb_init['config'] = config
+    train_tcn(win_x, win_y, test_data_dict, wandb_init)
+
+
+def train_and_log_baselines():
+    # best TCN hyperparameters found using the source dataset
+    config = dict(learning_rate=1e-3,
+                  dropout_rate=0.1,
+                  loss_function='mse',
+                  epochs=4,
+                  batch_size=64,
+                  validation_split=0.1,
+                  early_stop_patience=15,
+                  # seed=list(range(5)),
+                  seed=4,
+                  tcn=dict(dilations=2,
+                           filters=128),
+                  tcn2=True,
+                  kernel_size=3,
+                  transpose_input=True,
+                  save_model=False,
+                  l2_reg=1e-2,
+                  # train_dataset=['src', 'tar1', 'tar2', 'tar3'])
                   train_dataset='tar1')
 
     wandb_init = dict(
