a63c641c018acbcbaff005bc72425c3e55d8e108
luis <l.p.silvestrin@vu.nl>
2022-09-12 13:35:17 +0200

    fix build_tcn_from_config always marking the first tcn as return_sequences=true

--- utils/train_keras_tcn.py
+++ utils/train_keras_tcn.py
@@ -35,7 +35,7 @@ def r2_keras(y_true, y_pred):
 def build_tcn_from_config(nb_features: int, nb_steps: int, nb_out: int, config: SimpleNamespace):
     i = Input(shape=(nb_steps, nb_features))
 
-    return_sequences = True if config.tcn2 else True
+    return_sequences = True if config.tcn2 else False
     dilations = np.exp2(np.arange(config.tcn['dilations'])).astype('int').tolist()
     l2 = config.l2_reg if 'l2_reg' in vars(config).keys() else 0
     l2_reg = keras.regularizers.L2(l2)
