f1bbf026d53c870d98ad60cea8b9405cfdfc1458
luis <l.p.silvestrin@vu.nl>
2022-09-15 11:34:32 +0200

    running NN based tradaboost

--- algorithms/train_tradaboost.py
+++ algorithms/train_tradaboost.py
@@ -4,6 +4,7 @@ import random
 import keras
 
 import wandb
+from omegaconf import DictConfig
 from sklearn.model_selection import train_test_split
 
 from adapt.instance_based import TwoStageTrAdaBoostR2
@@ -12,6 +13,9 @@ from utils.train_keras_tcn import (
     restore_wandb_config,
     build_tcn_from_config
 )
+
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Dense, Dropout
 
 
 def train_tradaboost_tcn(src_x, src_y, tar_x, tar_y, test_sets, wandb_init):
@@ -35,7 +39,9 @@ def train_tradaboost_tcn(src_x, src_y, tar_x, tar_y, test_sets, wandb_init):
     nb_features = tr_x.shape[2]
     nb_steps = tr_x.shape[1]
     nb_out = 1
-
+    src_config.tcn2 = False
+    src_config.tcn.nb_filters = 4
+    src_config.tcn.dilations = 1
     model = build_tcn_from_config(nb_features, nb_steps, nb_out, src_config)
 
     opt = keras.optimizers.Adam(lr=src_config.learning_rate)
@@ -95,6 +101,78 @@ def train_tradaboost_tcn(src_x, src_y, tar_x, tar_y, test_sets, wandb_init):
     return model
 
 
+def train_tradaboost_nn(src_x, src_y, tar_x, tar_y, test_sets, wandb_init):
+    run = wandb.init(**wandb_init)
+    config = wandb.config
+    mlp_config = DictConfig(config.mlp)
+
+    np.random.seed(config.seed)
+    tf.random.set_seed(config.seed)
+    random.seed(config.seed)
+
+    rand_state = np.random.RandomState(config.seed)
+    tr_x, val_x, tr_y, val_y = train_test_split(tar_x, tar_y,
+                                                test_size=config.validation_split,
+                                                random_state=rand_state)
+
+    nb_features = tr_x.shape[1]
+    nb_out = 1
+
+    model = build_mlp(nb_features, nb_out, mlp_config)
+
+    opt = keras.optimizers.Adam(lr=mlp_config.learning_rate)
+    model.compile(loss=mlp_config.loss_function, optimizer=opt)
+
+    early_stop = keras.callbacks.EarlyStopping(
+        monitor="loss",
+        patience=mlp_config.early_stop_patience,
+        mode="min",
+        restore_best_weights=True,
+    )
+
+    # callbacks = []
+    callbacks = [early_stop]
+    # if 'save_model' in config.keys():
+    #     save_model = config.save_model
+    # else:
+    #     save_model = False
+    # callbacks.append(WandbCallback(save_model=save_model,
+    #                                save_graph=save_model))
+
+    tradaboost = TwoStageTrAdaBoostR2(estimator=model,
+                                      Xt=tr_x,
+                                      yt=tr_y,
+                                      cv=config.cv,
+                                      n_estimators=config.n_estimators,
+                                      n_estimators_fs=config.n_estimators_fs,
+                                      random_state=config.seed)
+
+    tradaboost.fit(src_x, src_y,
+                   epochs=mlp_config.epochs,
+                   batch_size=mlp_config.batch_size,
+                   callbacks=callbacks,
+                   verbose=1)
+
+    metrics = [tf.keras.metrics.RootMeanSquaredError(name='root_mean_squared_error'),
+               tf.keras.metrics.MeanAbsoluteError(name='mae'),
+               tf.keras.metrics.MeanSquaredError(name='loss')]
+    metrics_names = [m.name for m in metrics]
+
+    result = evaluate(val_x, val_y, tradaboost, metrics)
+    test_metric_names = ["val/" + n for n in metrics_names]
+    run.log(dict(zip(test_metric_names, result)))
+
+    for key, t_set in test_sets.items():
+        tst_x, tst_y = t_set
+        result = evaluate(tst_x, tst_y, tradaboost, metrics)
+        test_metric_names = [key + "/" + n for n in metrics_names]
+        run.log(dict(zip(test_metric_names, result)))
+
+    wandb.finish()
+
+    return model
+
+
 def evaluate(x, y, model, metrics):
     pred = model.predict(x)
     result = []
@@ -103,3 +181,22 @@ def evaluate(x, y, model, metrics):
         result.append(met.result().numpy())
         met.reset_state()
     return result
+
+
+def build_mlp(nb_features: int, nb_out: int, config: DictConfig):
+    i = Input(shape=(nb_features))
+
+    l2 = config.l2_reg if 'l2_reg' in config else 0
+    l2_reg = keras.regularizers.L2(l2)
+
+    m = i
+    for n in config.hidden:
+        m = Dense(n,
+                  activation='relu',
+                  kernel_regularizer=l2_reg)(m)
+        Dropout(config.dropout_rate)(m)
+
+    m = Dense(nb_out,
+              activation='linear',
+              kernel_regularizer=l2_reg)(m)
+    return Model(inputs=[i], outputs=[m])
--- experiments/transfer_learning_sota.py
+++ experiments/transfer_learning_sota.py
@@ -1,19 +1,18 @@
 import wandb
-from algorithms.train_tradaboost import train_tradaboost_tcn
+from algorithms.train_tradaboost import train_tradaboost_tcn, train_tradaboost_nn
 
 from utils.nasa_data_preprocess import load_preproc_data
 
 
-def sweep_tradaboost():
+def sweep_tradaboost_tcn():
     config = dict(lr=1,
-                  loss_function='mse',
                   validation_split=0.1,
                   # seed=list(range(5)),
                   seed=4,
-                  cv=3,
+                  cv=1,
                   n_estimators=2,
                   n_estimators_fs=2,
-                  src_run_path='transfer-learning-tcn/test2/17on4u6k',
+                  src_run_path='transfer-learning-tcn/test2/2mjc4t5x',
                   # train_dataset=['src', 'tar1', 'tar2', 'tar3'])
                   train_dataset='tar1')
 
@@ -42,5 +41,52 @@ def sweep_tradaboost():
     train_tradaboost_tcn(src_x, src_y, tar_x, tar_y, test_data_dict, wandb_init)
 
 
+def sweep_tradaboost_nn():
+    config = dict(
+        validation_split=0.1,
+      # seed=list(range(5)),
+      seed=4,
+      cv=1,
+      n_estimators=2,
+      n_estimators_fs=2,
+      mlp=dict(
+          learning_rate=0.001,
+          epochs=2,
+          batch_size=64,
+          early_stop_patience=15,
+          hidden=[1024, 1024, 1024],
+          l2_reg=1e-4,
+          loss_function='mse',
+          dropout_rate=0.1
+      ),
+      # train_dataset=['src', 'tar1', 'tar2', 'tar3'])
+      train_dataset='tar1'
+    )
+
+    wandb_init = dict(
+        project='test2',
+        entity='transfer-learning-tcn',
+        reinit=False,
+        config=config
+    )
+
+    wandb.init(**wandb_init)
+    config = wandb.config
+    print(config)
+    wandb_init['config'] = config
+
+    test_data_dict = dict()
+    train_data_dict = dict()
+    for name in [config.train_dataset, 'src']:
+        data_dict = load_preproc_data(name=name)
+        test_data_dict[name] = (data_dict['man_x_test'], data_dict['y_test'].reshape(-1, 1))
+        train_data_dict[name] = (data_dict['man_x_train'], data_dict['y_train'].reshape(-1, 1))
+
+    test_data_dict["test"] = test_data_dict[config.train_dataset]
+    src_x, src_y = train_data_dict['src']
+    tar_x, tar_y = train_data_dict[config.train_dataset]
+    train_tradaboost_nn(src_x, src_y, tar_x, tar_y, test_data_dict, wandb_init)
+
+
 if __name__ == '__main__':
-    sweep_tradaboost()
+    sweep_tradaboost_nn()
