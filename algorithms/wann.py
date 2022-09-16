a9491418c47d97fef7a79f39b393f13be292e05b
luis <l.p.silvestrin@vu.nl>
2022-09-16 14:52:06 +0200

    implement function to train tcn version of wann

--- algorithms/wann.py
+++ algorithms/wann.py
@@ -8,6 +8,7 @@ from adapt.instance_based import WANN
 from omegaconf import DictConfig
 from sklearn.model_selection import train_test_split
 
+from utils.train_keras_tcn import restore_wandb_config, build_tcn_from_config
 from utils.utils import evaluate, build_mlp
 
 
@@ -67,6 +68,77 @@ def train_wann(src_x, src_y, tar_x, tar_y, test_sets, wandb_init):
 
     for key, t_set in test_sets.items():
         tst_x, tst_y = t_set
+        result = evaluate(tst_x, tst_y, wann, metrics)
+        test_metric_names = [key + "/" + n for n in metrics_names]
+        run.log(dict(zip(test_metric_names, result)))
+
+    wandb.finish()
+
+    return model
+
+
+def train_wann_tcn(src_x, src_y, tar_x, tar_y, test_sets, wandb_init):
+    run = wandb.init(**wandb_init)
+    config = wandb.config
+
+    np.random.seed(config.seed)
+    tf.random.set_seed(config.seed)
+    random.seed(config.seed)
+
+    if config.transpose_input:
+        src_x = np.transpose(src_x, (0, 2, 1))
+        tar_x = np.transpose(tar_x, (0, 2, 1))
+
+    rand_state = np.random.RandomState(config.seed)
+    tr_x, val_x, tr_y, val_y = train_test_split(tar_x, tar_y,
+                                                test_size=config.validation_split,
+                                                random_state=rand_state)
+
+    nb_features = tar_x.shape[2]
+    nb_steps = tar_x.shape[1]
+    nb_out = 1
+
+    model = build_tcn_from_config(nb_features, nb_steps, nb_out, config)
+
+    opt = keras.optimizers.Adam(learning_rate=config.learning_rate)
+
+    early_stop = keras.callbacks.EarlyStopping(
+        monitor="loss",
+        patience=config.early_stop_patience,
+        mode="min",
+        restore_best_weights=True,
+    )
+
+    # callbacks = []
+    callbacks = [early_stop]
+
+    wann = WANN(task=model,
+                weighter=model,
+                Xt=tr_x,
+                yt=tr_y)
+
+    metrics = [tf.keras.metrics.RootMeanSquaredError(name='root_mean_squared_error'),
+               tf.keras.metrics.MeanAbsoluteError(name='mae'),
+               tf.keras.metrics.MeanSquaredError(name='loss')]
+
+    wann.compile(loss=config.loss_function, optimizer=opt, metrics={'disc': [], 'task': metrics})
+
+    wann.fit(src_x, src_y,
+             epochs=config.epochs,
+             batch_size=config.batch_size,
+             callbacks=callbacks,
+             verbose=1)
+
+    metrics_names = [m.name for m in metrics]
+
+    result = evaluate(val_x, val_y, wann, metrics)
+    test_metric_names = ["val/" + n for n in metrics_names]
+    run.log(dict(zip(test_metric_names, result)))
+
+    for key, t_set in test_sets.items():
+        tst_x, tst_y = t_set
+        if config.transpose_input:
+            tst_x = np.transpose(tst_x, (0, 2, 1))
         result = evaluate(tst_x, tst_y, wann, metrics)
         test_metric_names = [key + "/" + n for n in metrics_names]
         run.log(dict(zip(test_metric_names, result)))
