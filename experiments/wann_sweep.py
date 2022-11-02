8f02d88b2ee4ad5c5593fe9ae70577bf4b4d4b07
luis <l.p.silvestrin@vu.nl>
2022-11-02 18:03:18 +0100

    refactor to work with different source datasets

--- experiments/wann_sweep.py
+++ experiments/wann_sweep.py
@@ -23,7 +23,8 @@ def sweep_vanilla_wann():
           dropout_rate=0.1
         ),
         # train_dataset=['src', 'tar1', 'tar2', 'tar3'])
-        train_dataset='tar1',
+        train_dataset='bpm10_tar',
+        src_dataset='bpm10_src',
         C=0.5,
         pre_train=True
     )
@@ -42,13 +43,13 @@ def sweep_vanilla_wann():
 
     test_data_dict = dict()
     train_data_dict = dict()
-    for name in [config.train_dataset, 'src']:
+    for name in [config.train_dataset, config.src_dataset]:
         data_dict = load_preproc_data(name=name)
         test_data_dict[name] = (data_dict['man_x_test'], data_dict['y_test'].reshape(-1, 1))
         train_data_dict[name] = (data_dict['man_x_train'], data_dict['y_train'].reshape(-1, 1))
 
     test_data_dict["test"] = test_data_dict[config.train_dataset]
-    src_x, src_y = train_data_dict['src']
+    src_x, src_y = train_data_dict[config.src_dataset]
     tar_x, tar_y = train_data_dict[config.train_dataset]
     train_wann(src_x, src_y, tar_x, tar_y, test_data_dict, wandb_init)
 
