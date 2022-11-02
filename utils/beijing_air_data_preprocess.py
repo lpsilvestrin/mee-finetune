c5d7458ff2898702f9b4b45e575436d0b81821a5
luis <l.p.silvestrin@vu.nl>
2022-11-02 11:23:04 +0100

    preprocess based on tsai beijing dataset

--- utils/beijing_air_data_preprocess.py
+++ utils/beijing_air_data_preprocess.py
@@ -1,83 +1,63 @@
 import pandas as pd
 import os
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+from tsai.data.external import get_Monash_regression_data
+
+from nasa_data_preprocess import extract_manual_features
 
 
 def load_train(data_path="/home/luis/datasets/beijing_air_quality/"):
+    X, y, _ = get_Monash_regression_data("BeijingPM10Quality", path="", split_data=False)
+
+
+def preprocess_date(x_train, x_test, y_train, y_test, scaler=None):
+    # flatten windows
+    _, channels, win_size = x_train.shape
+    x_train = x_train.transpose(0,2,1).reshape(-1, channels)
+    x_test = x_test.transpose(0,2,1).reshape(-1, channels)
+
+    # normalize the data per input channel using training samples
+    if scaler is None:
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    # unflatten the time windows (samples, window size, channels)
+    x_train = x_train.reshape(-1, win_size, channels)
+    x_test = x_test.reshape(-1, win_size, channels)
+    return x_train, x_test, y_train, y_test
+
+
+def save_tl_datasets(path="../Data/"):
     """
-    load all the csv files from beijing air quality dataset and concatenate them on the same dataframe
-    dataset link: https://archive.ics.uci.edu/ml/datasets/Beijing+Multi-Site+Air-Quality+Data
+    load the beijing PM10 dataset and split the first 5000 samples as source and last 5000 as target datasets
+    split each dataset in training and test data and save them in the Data directory
+    the windowed version of the data is stored as (samples, timesteps, channels)
 
     """
-    file_list = os.listdir(data_path)
-    dfs = [pd.read_csv(os.path.join(data_path, fn)) for fn in file_list if fn[-4:] == ".csv"]
-    df = pd.concat(dfs)
-    return df
+    dsid = "BeijingPM10Quality"
+
+    X, y, _ = get_Monash_regression_data(dsid, path=path, split_data=False)
+    src_x, src_y = X[:5000, :, :], y[:5000]
+    tar_x, tar_y = X[-5000:, :, :], y[-5000:]
+    src = train_test_split(src_x, src_y, test_size=1000, shuffle=False)
+    tar = train_test_split(tar_x, tar_y, test_size=4500, shuffle=False)
+
+    preproc_dfs = [('src', src), ('tar', tar)]
+    for name, dfs in preproc_dfs:
+        tr_x, tst_x, tr_y, tst_y = preprocess_date(*dfs)
+        np.savez(os.path.join(path, dsid, f"{name}.npz"),
+                 win_x_train=tr_x,
+                 man_x_train=extract_manual_features(tr_x.transpose(0, 2, 1)),
+                 y_train=tr_y,
+                 win_x_test=tst_x,
+                 man_x_test=extract_manual_features(tst_x.transpose(0, 2, 1)),
+                 y_test=tst_y)
 
 
-def preprocess_data(df, nb_train_engines=80):
-    """
-    TODO 
-    1 - drop sensors 1, 5, 6, 10, 16, 18, 19, operational settings and other columns not used for modelling
-    2 - shuffle the engine ids and separate train and test sets
-    3 - normalize the sensors based on the training observations
-    4 - extract the time windows
-    5 - drop id column
-    6 - drop windows containing nan values
-    7 - separate the last RUL value for each window as a label
-    8 - drop the RUL column
-    Args:
-        df:
-        nb_train_engines: total engines which will be sorted for training
-
-    Returns:
-
-    """
-
-    op_set = ["op" + str(i) for i in range(1, 4)]
-    sensor = np.array(["sensor" + str(i) for i in range(1, 22)])
-    drop_cols = list(sensor[np.array([1,5,6,10,16,18,19])-1])
-    drop_cols = drop_cols + op_set + ['cycle']
-
-    df = df.drop(drop_cols, axis=1)
-
-    # sample engines for training and for test
-    ids = df["id"].unique()
-    # create a random state for reproducibility
-    rand = np.random.RandomState(1234)
-    rand.shuffle(ids)
-    df_train = df[df["id"].isin(ids[:nb_train_engines])].reset_index(drop=True)
-    y_train = df_train.pop('RUL')
-    id_train = df_train.pop('id')
-    df_test = df[df["id"].isin(ids[nb_train_engines:])].reset_index(drop=True)
-    y_test = df_test.pop('RUL')
-    id_test = df_test.pop('id')
-    # save input column names
-    x_cols = df_train.columns
-
-    # normalize the data
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    pt = PowerTransformer()
-    df_train = scaler.fit_transform(df_train)
-    df_train = pt.fit_transform(df_train)
-    df_train = pd.DataFrame(df_train, columns=x_cols)
-    df_train['RUL'] = y_train
-    df_train['id'] = id_train
-    df_test = scaler.transform(df_test)
-    df_test = pt.transform(df_test)
-    df_test = pd.DataFrame(df_test, columns=x_cols)
-    df_test['RUL'] = y_test
-    df_test['id'] = id_test
-
-    # extract the time windows and separate RUL as the label
-    win = SlidingWindowPanel(window_len=30,
-                             unique_id_cols=['id'],
-                             get_x=x_cols,
-                             get_y=['RUL'],
-                             horizon=0,
-                             stride=1)
-
-    win_train, rul_train = win(df_train)
-    win_test, rul_test = win(df_test)
-
-    return win_train, rul_train, win_test, rul_test
+if __name__ == '__main__':
+    save_tl_datasets()
