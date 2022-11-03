4bdb629c983df3de00a5f7dbc46e7bc29cef9ad8
luis <l.p.silvestrin@vu.nl>
2022-11-03 15:43:48 +0100

    refactor to work with different source datasets

--- experiments/tradaboost_nn_sweep.py
+++ experiments/tradaboost_nn_sweep.py
@@ -6,12 +6,12 @@ from utils.datasets import load_preproc_data
 def sweep_tradaboost_nn():
     config = dict(
         validation_split=0.1,
-      # seed=list(range(5)),
-      seed=4,
-      cv=1,
-      n_estimators=2,
-      n_estimators_fs=2,
-      mlp=dict(
+        # seed=list(range(5)),
+        seed=4,
+        cv=1,
+        n_estimators=2,
+        n_estimators_fs=2,
+        mlp=dict(
           learning_rate=0.001,
           epochs=2,
           batch_size=64,
@@ -20,9 +20,10 @@ def sweep_tradaboost_nn():
           l2_reg=1e-4,
           loss_function='mse',
           dropout_rate=0.1
-      ),
-      # train_dataset=['src', 'tar1', 'tar2', 'tar3'])
-      train_dataset='tar1'
+        ),
+        # train_dataset=['src', 'tar1', 'tar2', 'tar3'])
+        train_dataset='bpm10_tar',
+        src_dataset='bpm10_src'
     )
 
     wandb_init = dict(
@@ -39,13 +40,13 @@ def sweep_tradaboost_nn():
 
     test_data_dict = dict()
     train_data_dict = dict()
-    for name in [config.train_dataset, 'src']:
+    for name in [config.train_dataset, config.src_dataset]:
         data_dict = load_preproc_data(name=name)
         test_data_dict[name] = (data_dict['man_x_test'], data_dict['y_test'].reshape(-1, 1))
         train_data_dict[name] = (data_dict['man_x_train'], data_dict['y_train'].reshape(-1, 1))
 
     test_data_dict["test"] = test_data_dict[config.train_dataset]
-    src_x, src_y = train_data_dict['src']
+    src_x, src_y = train_data_dict[config.src_dataset]
     tar_x, tar_y = train_data_dict[config.train_dataset]
     train_tradaboost_nn(src_x, src_y, tar_x, tar_y, test_data_dict, wandb_init)
 
