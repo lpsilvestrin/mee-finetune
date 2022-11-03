d8e9f8cce744952c5e0800539303b31ce4d2d2bb
luis <l.p.silvestrin@vu.nl>
2022-11-03 15:14:21 +0100

    refactor to work with different source datasets

--- experiments/wann_tcn_sweep.py
+++ experiments/wann_tcn_sweep.py
@@ -22,7 +22,8 @@ def sweep_wann_tcn():
         transpose_input=True,
         save_model=False,
         l2_reg=1e-2,
-        train_dataset='tar1'
+        train_dataset='bpm10_tar',
+        src_dataset='bpm10_src'
     )
 
     wandb_init = dict(
@@ -39,13 +40,13 @@ def sweep_wann_tcn():
 
     test_data_dict = dict()
     train_data_dict = dict()
-    for name in [config.train_dataset, 'src']:
+    for name in [config.train_dataset, config.src_dataset]:
         data_dict = load_preproc_data(name=name)
         test_data_dict[name] = (data_dict['win_x_test'], data_dict['y_test'].reshape(-1, 1))
         train_data_dict[name] = (data_dict['win_x_train'], data_dict['y_train'].reshape(-1, 1))
 
     test_data_dict["test"] = test_data_dict[config.train_dataset]
-    src_x, src_y = train_data_dict['src']
+    src_x, src_y = train_data_dict[config.src_dataset]
     tar_x, tar_y = train_data_dict[config.train_dataset]
     train_wann_tcn(src_x, src_y, tar_x, tar_y, test_data_dict, wandb_init)
 
