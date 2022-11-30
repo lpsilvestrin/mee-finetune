a0db932251269f3bcad73b14ada9ef169efd2179
luis <l.p.silvestrin@vu.nl>
2022-11-30 13:12:16 +0100

    debuging

--- utils/custom_loss_keras_model.py
+++ utils/custom_loss_keras_model.py
@@ -185,12 +185,12 @@ def reyi_entropy(x: tf.Tensor, sigma: float):
 
     k = calculate_gram_mat(x, sigma)
     # print("gram_mat:", k)
-    # print("trace gram:", tf.linalg.trace(k))
+    print("trace gram:", tf.linalg.trace(k))
     k = k / tf.linalg.trace(k)
 
     # print("norm_gram:", k)
 
-    eigv = tf.abs(tf.linalg.eigvh(k)[0])
+    eigv = tf.abs(tf.linalg.eigh(k)[0])
     eig_pow = eigv ** alpha
     entropy = (1 / (1 - alpha)) * tf_log2(tf.reduce_sum(eig_pow))
 
