ec5c7e077e908ec3bb59c978909608aa8c3e7dc2
luis <l.p.silvestrin@vu.nl>
2022-12-01 18:48:44 +0100

    testing custom loss torch

--- experiments/custom_loss_keras_model_sweep.py
+++ experiments/custom_loss_keras_model_sweep.py
@@ -2,58 +2,60 @@ import numpy as np
 import wandb
 
 from utils.custom_loss_keras_model import train_custom_loss_tcn
+from utils.custom_loss_torch import train_custom_loss_tcn as train_torch
 from utils.datasets import load_preproc_data
 
 
 def sweep_custom_keras_model():
     # best TCN hyperparameters found using the source dataset
+    config = dict(
+        learning_rate=1e-4,
+        dropout_rate=0.1,
+        loss_function='mse',
+        epochs=1,
+        batch_size=64,
+        validation_split=0.1,
+        early_stop_patience=80,
+        # seed=list(range(5)),
+        seed=7,
+        # tcn=dict(dilations=2,
+        #        filters=128),
+        tcn2=True,
+        filters=[128, 128],
+        kernel_size=3,
+        transpose_input=True,
+        save_model=False,
+        l2_reg=1e-5,
+        train_dataset='src',
+        test_dataset=[],
+        trunc_label=False,
+        debug_mode=False,
+        input_type='win',
+        model_type='tcn'
+    )
+
+    # test mlp hyperparameters
     # config = dict(
-    #     learning_rate=1e-4,
+    #     learning_rate=1e-3,
     #     dropout_rate=0.1,
     #     loss_function='MEE',
-    #     epochs=200,
-    #     batch_size=64,
+    #     epochs=400,
+    #     batch_size=32,
     #     validation_split=0.1,
-    #     early_stop_patience=80,
+    #     early_stop_patience=400,
     #     # seed=list(range(5)),
-    #     seed=7,
-    #     tcn=dict(dilations=2,
-    #            filters=128),
-    #     tcn2=True,
-    #     kernel_size=3,
-    #     # transpose_input=False,
+    #     seed=3,
+    #     hidden=[200, 20],
+    #     transpose_input=False,
     #     save_model=False,
     #     l2_reg=1e-5,
     #     train_dataset='src',
     #     test_dataset=[],
     #     trunc_label=False,
     #     debug_mode=False,
-    #     input_type='win',
-    #     model_type='tcn'
+    #     input_type='c22',
+    #     model_type='mlp'
     # )
-
-    # test mlp hyperparameters
-    config = dict(
-        learning_rate=1e-4,
-        dropout_rate=0.1,
-        loss_function='MEE',
-        epochs=400,
-        batch_size=32,
-        validation_split=0.1,
-        early_stop_patience=400,
-        # seed=list(range(5)),
-        seed=3,
-        hidden=[50],
-        transpose_input=False,
-        save_model=False,
-        l2_reg=1e-5,
-        train_dataset='src',
-        test_dataset=[],
-        trunc_label=False,
-        debug_mode=True,
-        input_type='man',
-        model_type='mlp'
-    )
 
     wandb_init = dict(
         project='test2',
@@ -69,7 +71,8 @@ def sweep_custom_keras_model():
     win_x, win_y, test_data_dict = prepare_data(config)
 
     wandb_init['config'] = config
-    train_custom_loss_tcn(win_x, win_y, test_data_dict, wandb_init)
+    # train_custom_loss_tcn(win_x, win_y, test_data_dict, wandb_init)
+    train_torch(win_x, win_y, test_data_dict, wandb_init)
 
 
 def prepare_data(config):
