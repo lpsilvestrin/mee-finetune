299b3a8f96716b2462eb81bae16670834859a22a
luis <l.p.silvestrin@vu.nl>
2022-12-21 14:14:48 +0100

    rename train function

--- experiments/custom_loss_torch_sweep.py
+++ experiments/custom_loss_torch_sweep.py
@@ -5,60 +5,60 @@ import wandb
 from utils.data_utils import prepare_data
 
 from utils.custom_loss_keras_model import train_custom_loss_keras_model
-from utils.custom_loss_torch import train_custom_loss_tcn as train_torch
+from utils.custom_loss_torch import train_torch
 from utils.datasets import load_preproc_data
 
 
 def sweep_custom_torch_model():
     # best TCN hyperparameters found using the source dataset
+    # config = dict(
+    #     learning_rate=1e-4,
+    #     dropout_rate=0.1,
+    #     loss_function='MEE',
+    #     epochs=10,
+    #     batch_size=64,
+    #     validation_split=0.1,
+    #     early_stop_patience=2,
+    #     # seed=list(range(5)),
+    #     seed=7,
+    #     tcn=dict(dilations=2,
+    #              filters=128),
+    #     tcn2=False,
+    #     # filters=[128, 128],
+    #     kernel_size=3,
+    #     transpose_input=True,
+    #     save_model=False,
+    #     l2_reg=1e-5,
+    #     train_dataset='src',
+    #     test_dataset=[],
+    #     norm_label=True,
+    #     debug_mode=False,
+    #     input_type='win',
+    #     model_type='tcn'
+    # )
+
+    # test mlp hyperparameters
     config = dict(
         learning_rate=1e-4,
         dropout_rate=0.1,
         loss_function='MEE',
-        epochs=10,
-        batch_size=64,
+        epochs=400,
+        batch_size=32,
         validation_split=0.1,
-        early_stop_patience=2,
+        early_stop_patience=400,
         # seed=list(range(5)),
-        seed=7,
-        tcn=dict(dilations=2,
-                 filters=128),
-        tcn2=False,
-        # filters=[128, 128],
-        kernel_size=3,
-        transpose_input=True,
+        seed=3,
+        hidden=[],
+        transpose_input=False,
         save_model=False,
         l2_reg=1e-5,
         train_dataset='src',
         test_dataset=[],
         trunc_label=False,
         debug_mode=False,
-        input_type='win',
-        model_type='tcn'
+        input_type='man',
+        model_type='mlp'
     )
-
-    # test mlp hyperparameters
-    # config = dict(
-    #     learning_rate=1e-3,
-    #     dropout_rate=0.1,
-    #     loss_function='MEE',
-    #     epochs=400,
-    #     batch_size=32,
-    #     validation_split=0.1,
-    #     early_stop_patience=400,
-    #     # seed=list(range(5)),
-    #     seed=3,
-    #     hidden=[200, 20],
-    #     transpose_input=False,
-    #     save_model=False,
-    #     l2_reg=1e-5,
-    #     train_dataset='src',
-    #     test_dataset=[],
-    #     trunc_label=False,
-    #     debug_mode=False,
-    #     input_type='man',
-    #     model_type='mlp'
-    # )
 
     wandb_init = dict(
         project='test2',
