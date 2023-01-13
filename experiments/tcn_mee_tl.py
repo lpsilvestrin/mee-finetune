dc1493c0a95c45e5b0523e4ea16b656a7cbca6c7
luis <l.p.silvestrin@vu.nl>
2023-01-13 10:19:11 +0100

    run one seed to compute residuals

--- experiments/tcn_mee_tl.py
+++ experiments/tcn_mee_tl.py
@@ -9,7 +9,7 @@ from utils.data_utils import prepare_data
 from utils.custom_loss_torch import train_torch
 
 
-def run(loss_src='MEE', loss_tar='mse', seed=0):
+def run(loss_src='MEE', loss_tar='mse', seed=0, tar_data="tar1", src_data="src"):
     # test mlp hyperparameters
     config = dict(
         learning_rate=1e-4,
@@ -34,12 +34,12 @@ def run(loss_src='MEE', loss_tar='mse', seed=0):
         model_type='tcn'
     )
 
-    config['group'] = 'bpm10_src'
+    config['group'] = src_data
     config['loss_function'] = loss_src
-    config['train_dataset'] = 'bpm10_src'
+    config['train_dataset'] = src_data
 
     wandb_init = dict(
-        project='tcn_custom_loss',
+        project='tcn_custom_loss_residuals',
         entity='transfer-learning-tcn',
         reinit=False,
         config=config
@@ -55,11 +55,11 @@ def run(loss_src='MEE', loss_tar='mse', seed=0):
     litmodel = train_torch(train_x, train_y, test_data_dict, wandb_init)
 
     # finetune on the target data
-    config['group'] = 'bpm10_tar'
+    config['group'] = tar_data
     config['loss_function'] = loss_tar
     config['loss_src'] = loss_src
-    config['train_dataset'] = 'bpm10_tar'
-    config['test_dataset'] = ['bpm10_tar']
+    config['train_dataset'] = tar_data
+    # config['test_dataset'] = [tar_data]
     wandb_init['config'] = config
 
     train_x, train_y, test_data_dict = prepare_data(DictConfig(config))
@@ -68,6 +68,9 @@ def run(loss_src='MEE', loss_tar='mse', seed=0):
 
 
 if __name__ == '__main__':
-    params = product(['MEE', 'mse'], ['MEE', 'mse'], list(range(0,20)))
-    for loss_src, loss_tar, seed in params:
-        run(loss_src=loss_src, loss_tar=loss_tar, seed=seed)
+    data_pairs = product(['src'], ['tar1', 'tar2', 'tar3'])
+    data_pairs.append(['bpm10_src', 'bpm10_tar'])
+    params = product(['MEE', 'mse'], ['MEE', 'mse'], list(range(0,1)), data_pairs)
+    for loss_src, loss_tar, seed, src_tar_pair in params:
+        src, tar = src_tar_pair
+        run(loss_src=loss_src, loss_tar=loss_tar, seed=seed, src_data=src, tar_data=tar)
