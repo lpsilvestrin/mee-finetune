4cd40212147995b9a293096e6661f9b2d62609ec
luis <l.p.silvestrin@vu.nl>
2023-02-21 18:16:21 +0100

    fix imports

--- experiments/distribution_shift_simulation.py
+++ experiments/distribution_shift_simulation.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-from lightning_lite import seed_everything
+from pytorch_lightning import seed_everything
 from pytorch_lightning import Trainer, LightningModule
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
