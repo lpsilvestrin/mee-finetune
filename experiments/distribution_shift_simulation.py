0d712415ed40aff9bf144f6c8e489900c26685c9
luis <l.p.silvestrin@vu.nl>
2023-02-21 18:14:56 +0100

    run simulation inspired by exp from hsic paper

--- experiments/distribution_shift_simulation.py
+++ experiments/distribution_shift_simulation.py
@@ -4,10 +4,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 from lightning_lite import seed_everything
+from pytorch_lightning import Trainer, LightningModule
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from torch.nn import Linear
 from torch.utils.data import TensorDataset, DataLoader
+from torchmetrics.functional import mean_squared_error as tmse, mean_absolute_error as tmae
 
 from algorithms.custom_loss_torch_lit_class import loss_fn
 
@@ -25,7 +27,28 @@ def linear_simulation(n, xmean, xcov, std, slope, intercept):
     return x, y.reshape(-1, 1)
 
 
-def linear_regression_torch(x, y, xtst, num_epochs=100, learning_rate=0.1, loss_name='MEE'):
+def polinomial_simulation(n, xrange, std, slope, intercept):
+    """
+    simulate a polinomial relationship between x and y with non-gaussian noise
+    x is a univariate gaussian with mean xmean and covariance xcov
+
+    """
+    x = np.random.uniform(xrange[0], xrange[1], n)
+    order = len(slope)
+    x = np.array([x ** (i+1) for i in range(order)]).T
+
+    y = x.dot(slope) + intercept + np.random.laplace(0, std, n)
+    return x, y.reshape(-1, 1)
+
+
+def hsic_paper_simulation_exp(n, std, slope, x=None):
+    if x is None:
+        x = np.random.uniform(-1, 1, size=(n, 100))
+    y = x.dot(slope) + np.random.laplace(0, std, n)
+    return x, y.reshape(-1, 1)
+
+
+def linear_regression_torch(x, y, num_epochs=100, learning_rate=0.1, loss_name='MEE'):
     # Convert x and y to PyTorch tensors
     x_tensor = torch.tensor(x, dtype=torch.float32)
     y_tensor = torch.tensor(y, dtype=torch.float32)
@@ -34,7 +57,7 @@ def linear_regression_torch(x, y, xtst, num_epochs=100, learning_rate=0.1, loss_
     dataset = TensorDataset(x_tensor, y_tensor)
 
     # Create a dataloader to load the data in batches
-    dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
 
     # Initialize model parameters (slope and intercept)
     model = Linear(x.shape[1], y.shape[1])
@@ -50,7 +73,7 @@ def linear_regression_torch(x, y, xtst, num_epochs=100, learning_rate=0.1, loss_
             y_pred = model(batch_x)
 
             # Compute loss
-            batch_loss = loss_fn(batch_x, y_pred, batch_y, name=loss_name, s_y=2)
+            batch_loss = loss_fn(batch_x, y_pred, batch_y, name=loss_name, s_y=2, debug=False)
             epoch_loss += batch_loss.item()
 
             # Compute gradients
@@ -66,9 +89,25 @@ def linear_regression_torch(x, y, xtst, num_epochs=100, learning_rate=0.1, loss_
         if (epoch+1) % 10 == 0:
             print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, epoch_loss / len(dataloader)))
 
+    # compute bias
+    bias = 0
+    if loss_name == 'MEE':
+        with torch.no_grad():
+            y_pred = model(x_tensor)
+            bias = torch.mean(y_tensor - y_pred)
+
+    return model, bias
+
+
+def evaluate_model(model, xtst, ytst, bias=0):
+
     # Compute predictions
-    y_pred = model(torch.tensor(xtst, dtype=torch.float32))
-    return y_pred.float().detach().numpy()
+    with torch.no_grad():
+        y_pred = model(torch.tensor(xtst, dtype=torch.float32))
+        y_pred = y_pred + bias
+        mse = tmse(y_pred, torch.tensor(ytst, dtype=torch.float32)).item()
+
+    return mse
 
 
 def run_simulation():
@@ -84,28 +123,31 @@ def run_simulation():
     xmean = np.array([0, 0])
     xcov = np.array([[1, 0], [0, 1]])
     std = 1
-    slope = np.array([1, 1])
+    # slope = np.array([1, 1])
+    slope = np.random.normal(0, 0.1, 100)
     intercept = 5
     repetitions = 2
-    max_shift = 5
+    max_shift = 1
     res = []
-    for s in np.linspace(0, max_shift, 2):
+
+    x_train = np.random.normal(1, 0.1, size=(n_train, 100))
+
+    # train_data = [linear_simulation(n_train, xmean, xcov, std, slope, intercept) for _ in range(repetitions)]
+    train_data = [hsic_paper_simulation_exp(n_train, std, slope, x=x_train) for _ in range(repetitions)]
+    msl_models = [linear_regression_torch(x, y, num_epochs=100, learning_rate=1e-2, loss_name='mse') for x, y in train_data]
+    mee_models = [linear_regression_torch(x, y, num_epochs=200, learning_rate=1e-3, loss_name='MEE') for x, y in train_data]
+
+    for s in np.linspace(0, max_shift, 5):
         seed_everything(_SEED)
-        for i in range(repetitions):
-            x_train, y_train = linear_simulation(n_train, xmean, xcov, std, slope, intercept)
-            x_test, y_test = linear_simulation(n_test, xmean + s, xcov, std, slope, intercept)
-            # msl = LinearRegression().fit(x_train, y_train)
-            # error = mean_squared_error(y_test, msl.predict(x_test))
-            # res.append((s, error, 'MSL'))
-            y_pred = linear_regression_torch(x_train, y_train, x_test, num_epochs=100, loss_name='mse')
-            error = mean_squared_error(y_test, y_pred)
-            res.append((s, error, 'MSLt'))
-            y_pred = linear_regression_torch(x_train, y_train, x_test, num_epochs=100)
-            error = mean_squared_error(y_test, y_pred)
-            res.append((s, error, 'MEE'))
+        # x_test, y_test = linear_simulation(n_test, xmean + s, xcov, std, slope, intercept)
+        x_test, y_test = hsic_paper_simulation_exp(n_test, std, slope)
+        msl_res = [(s, evaluate_model(m, x_test, y_test, bias=0), 'MSL') for m, _ in msl_models]
+        mee_res = [(s, evaluate_model(m, x_test, y_test, bias=b), 'MEE') for m, b in mee_models]
+        res = res + msl_res + mee_res
 
     df = pd.DataFrame(res, columns=['shift', 'MSE', 'loss'])
     sns.lineplot(data=df, x='shift', y='MSE', hue='loss')
+    plt.savefig('../plots/linear_regression_shift.png')
     plt.show()
 
 
