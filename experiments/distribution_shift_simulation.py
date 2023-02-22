9ddd018f64bff8501d67bb53a6604c8db65d1a51
luis <l.p.silvestrin@vu.nl>
2023-02-22 14:57:19 +0100

    test plot markers

--- experiments/distribution_shift_simulation.py
+++ experiments/distribution_shift_simulation.py
@@ -152,7 +152,7 @@ def run_simulation():
         res = res + msl_res + mee_res + hsic_res
 
     df = pd.DataFrame(res, columns=['shift', 'MSE', 'loss'])
-    sns.lineplot(data=df, x='shift', y='MSE', hue='loss', style='loss')
+    sns.lineplot(data=df, x='shift', y='MSE', hue='loss', style='loss', markers=True, dashes=False)
     plt.savefig('../plots/linear_regression_shift.png')
     plt.show()
 
