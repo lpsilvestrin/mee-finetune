2ab7dc0fc141453e8a824ba4e6f387e8ccf9adf5
luis <l.p.silvestrin@vu.nl>
2023-05-01 13:52:39 +0200

    rename losses in plot

--- experiments/distribution_shift_simulation.py
+++ experiments/distribution_shift_simulation.py
@@ -151,9 +151,9 @@ def covariate_shift_sim():
     # train_data = [hsic_paper_simulation_exp(n_train, slope, x_train) for _ in range(repetitions)]
     train_data = [linsim(slope, gen.uniform(-1, 1, size=(n_train, 100)), gen_noise(n_train, noise_type)) for _ in range(repetitions)]
     seed_everything(_SEED+1)
-    msl_models = [linear_regression_torch(x, y, num_epochs=epochs, learning_rate=1e-4, loss_name='mse') for x, y in train_data]
+    mse_models = [linear_regression_torch(x, y, num_epochs=epochs, learning_rate=1e-4, loss_name='mse') for x, y in train_data]
     seed_everything(_SEED+1)
-    mal_models = [linear_regression_torch(x, y, num_epochs=epochs, learning_rate=1e-4, loss_name='MAE') for x, y in train_data]
+    mae_models = [linear_regression_torch(x, y, num_epochs=epochs, learning_rate=1e-4, loss_name='MAE') for x, y in train_data]
     seed_everything(_SEED+1)
     mee_models = [linear_regression_torch(x, y, num_epochs=epochs, learning_rate=1e-4, loss_name='MEE') for x, y in train_data]
     seed_everything(_SEED+1)
@@ -166,8 +166,8 @@ def covariate_shift_sim():
         # x_test, y_test = linear_simulation(n_test, xmean + s, xcov, std, slope, intercept)
         # x_test, y_test = linsim(slope, gen.normal(0, 1, size=(n_test, 100))+s, gen_noise(n_test, noise_type))
         test_data = [linsim(slope, gen.normal(0, 1, size=(n_test, 100))+s, gen_noise(n_test, noise_type)) for _ in range(repetitions)]
-        msl_res = [(s, evaluate_model(m, x_test, y_test, bias=0), 'MSL') for m, _ in msl_models for x_test, y_test in test_data]
-        mal_res = [(s, evaluate_model(m, x_test, y_test, bias=0), 'MAL') for m, _ in mal_models for x_test, y_test in test_data]
+        msl_res = [(s, evaluate_model(m, x_test, y_test, bias=0), 'MSE') for m, _ in mse_models for x_test, y_test in test_data]
+        mal_res = [(s, evaluate_model(m, x_test, y_test, bias=0), 'MAE') for m, _ in mae_models for x_test, y_test in test_data]
         mee_res = [(s, evaluate_model(m, x_test, y_test, bias=b), 'MEE') for m, b in mee_models for x_test, y_test in test_data]
         hsic_res = [(s, evaluate_model(m, x_test, y_test, bias=b), 'HSIC') for m, b in hsic_models for x_test, y_test in test_data]
         res = res + msl_res + mal_res + mee_res + hsic_res
